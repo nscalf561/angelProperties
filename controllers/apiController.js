596ba65b57d667ef2d17436238ab3c5e61dd95a0
nscalf561 <scalf.nicholas@gmail.com>
2016-09-27 14:40:27 -0700

    Setup documentation skeleton

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,7 +4,7 @@ let apiController = {
   index: (req, res) => {
     res.json({
       message: 'You made it to the api!',
-      documentation_url: 'TBD',
+      documentation_url: 'https://github.com/nscalf561/angelProperties',
       base_url: 'TBD',
       endpoints: [
         {method: 'GET', path: '/api', description: 'Document available endpoints'}
