239537632b7c329ca7f0ad89df97226e83feb8a0
nscalf561 <scalf.nicholas@gmail.com>
2016-10-01 15:13:15 -0700

    Add basic User Controller CRUD

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,64 @@ let app     = require('../server'),
 
 let userController = {
 
+  index : (req, res) => {
+    User.find({}, (err, users) => {
+      if (err) {
+        res.status(500).send(); //TODO propogate errors
+        console.log(`There was an error getting the index of users: ${err}`);
+      }
+      res.json({users: users});
+    });
+  },
 
+  findUserById : (req, res) => {
+    User.findOne({id: req.params.id}, (err, user) => {
+      if (err) {
+        res.status(500).send(); //TODO propogate errors
+        console.log(`There was an error getting the user by id: ${err}`);
+      }
+      res.json({user: user});
+    });
+  },
+
+  updateUser : (req, res) => {
+    User.findOne({id: req.params.id}, (err, user) => {
+      if (err) {
+        res.status(500).send(); //TODO propgate errors
+        console.log(`There was an error getting the user to update: ${err}`);
+      }
+
+      //TODO add sanitization for the updates, ie check if name is a string
+      if (req.body.name) { user.name = req.body.name; }
+      if (req.body.email) { user.email = req.body.email; }
+      if (req.body.password) { user.password = req.body.password; } //TODO probably dont do this at all here
+      if (req.body.role) { user.role = req.body.role; }
+      if (req.body.picture) { user.picture = req.body.picture; }
+      if (req.body.bio) { user.bio = req.body.bio; }
+      if (req.body.investorStatus) { user.investorStatus = req.body.investorStatus; }
+      if (req.body.builderStatus) { user.builderStatus = req.body.builderStatus; }
+      //TODO this will require much more logic, when does an investment become previous?
+      if (req.body.previousInvestments) { user.previousInvestments = req.body.previousInvestments; }
+
+      User.update({id: req.params.id}, user, (err, user) => {
+        if (err) {
+          res.status(500).send(); //TODO propgate error
+          console.log(`There was an error updating the user: ${err}`);
+        }
+        res.json({user: user});
+      });
+    });
+  },
+
+  deleteUserById : (req, res) => {
+    User.remove({id: req.params.id}, (err) => {
+      if (err) {
+        res.status(500).send(); //TODO propogate errors
+        console.log(`There was an error deleting the user by id (${req.params.id}): ${err}`);
+      }
+      res.json({message: "User deleted"});
+    });
+  }
 
 };
 
