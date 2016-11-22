2a877892ddd2cb6681bacca50f1eb74029b7ae6b
nscalf561 <scalf.nicholas@gmail.com>
2016-11-21 19:26:57 -0800

    Change test folder name to use default mocha recursive search

--- test/userTest.js
+++ test/userTest.js
@@ -6,11 +6,10 @@ var config = require('../config/config.js');
 mongoose.connect(config.database);
 
 describe('Project', function() {
-  describe('#save()', function() {
+  // describe('#save()', function() {
     it('project should save without error', function(done) {
       var project = new Project({name: 'Angel'});
       project.save(done);
     });
-  });
+  // });
 });
-
