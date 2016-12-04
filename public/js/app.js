2e13f15ba75c9b027c4e02752b1b6eac86c48d0b
nscalf561 <scalf.nicholas@gmail.com>
2016-12-03 19:57:37 -0800

    Include angular router, /#/login, and login form"

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,24 @@
-var app = angular.module("", []);
+var app = angular.module("AngelProperties", ['ngRoute']);
+
+app.config(($routeProvider) => {
+
+  $routeProvider
+
+    .when('/', {
+      templateUrl : 'templates/home.html',
+      // controller : 'mainController'
+    })
+
+    .when('/login', {
+      templateUrl : 'templates/login.html',
+      controller : 'loginController'
+    });
+
+});
+
+app.controller('loginController', ($scope) => {
+  $scope.loginForm = true;
+  $scope.switchForm = () => {
+    $scope.loginForm = $scope.loginForm ? false : true;
+  };
+});
